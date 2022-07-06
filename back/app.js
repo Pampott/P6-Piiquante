b00eeff3f0a6ae8bac8f3b03d6a6f0f99751fcf7
Paloma <celinipaloma@gmail.com>
2022-07-06 21:50:34 +0200

    test première route: bug bad request

--- back/app.js
+++ back/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('../routes/user');
+const userRoutes = require('./routes/user');
 
 const app = express();
 app.use(express.json());
