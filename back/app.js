27b31b4bc83edea82370fc347b45c0498b6221ae
Paloma <celinipaloma@gmail.com>
2022-07-06 16:00:20 +0200

    Création première route signup

---
+++ back/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('../routes/user');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://Pampott:Mimitoss@cluster0.qzfen.mongodb.net/?retryWrites=true&w=majority",
+{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("MongoDB connecté !"))
+.catch(() => console.log(("Connexion à MongoDB échouée. ")));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/auth/', userRoutes);
+/*
+
+
+
+
+app.get('/api/sauces', (req, res) => {
+
+});
+
+app.get('/api/sauces/:id', (req, res) => {
+
+});
+
+app.post('api/sauces', (req, res) => {
+
+});
+
+app.post('/api/auth/signup', (req, res) => {
+
+});
+
+app.post('api/auth/login', (req, res) => {
+
+});
+
+*/
+module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+//const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+exports.signup = (req, res) => {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user = new User({
+            email: req.body.email,
+            password: hash,
+        })
+
+        user.save()
+        .then(() => res.status(201).json({ message: "Compte créé !"}))
+        .catch((error) => res.status(400).json({ message: "Adresse mail déjà utilisée !"}));
+    })
+    .catch((error) => res.status(500).json({message: error}));
+};
+
+/*exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if(!user) {
+            return res.status(401).json( {message: "Identifiants incorrects"} )
+        } else {
+            return res.status(200).json({
+                userId: user._id,
+                token: user._id + "TOKEN",
+            })
+        }
+    })
+    .catch()
+}*/
