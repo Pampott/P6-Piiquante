2d66c344dfbee85d3f83ccf7c204ad879102319d
Paloma <celinipaloma@gmail.com>
2022-07-12 14:19:23 +0200

    all sauces controllers v1

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -27,4 +27,129 @@ exports.createSauce = (req, res, next) => {
     sauce.save()
     .then(() => res.status(201).json({message: "Nouvelle sauce ajoutée avec succès !"}))
     .catch(error => res.status(400).json({error}));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({
+      _id: req.params.id,
+    })
+      .then((sauce) => res.status(200).json(sauce))
+      .catch((error) => res.status(400).json({ error }));
+};
+
+exports.modifySauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+
+        const filename = sauce.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, () => {
+
+        const sauceObject = req.file
+          ? {
+              ...JSON.parse(req.body.sauce),
+              imageUrl: `${req.protocol}://${req.get("host")}/images/${
+                req.file.filename
+              }`,
+            }
+          :
+            { ...req.body };
+        Sauce.updateOne(
+          { _id: req.params.id },
+          { ...sauceObject, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Sauce mise à jour !" }))
+          .catch((error) => res.status(400).json({ error }));
+        });
+    });
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+        if (!sauce) {
+            res.status(404).json({
+            error: new Error("Cette sauce n'existe pas"),
+            });
+        }
+        if (sauce.userId !== req.auth.userId) {
+            res.status(401).json({
+            error: new Error(
+                "Vous ne pouvez pas supprimer une sauce dont vous n'êtres pas le propriétaire"
+            ),
+            });
+        }
+        const filename = sauce.imageUrl.split("/images/")[1];
+        fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: "Sauce supprimée" }))
+          .catch((error) =>res.status(400).json({ error }));
+        });
+    });
+};
+
+exports.likeSauce = (req, res, next) => {
+Sauce.findOne({ _id: req.params.id })
+.then((sauce) => {
+    if (!sauce) {
+    return res.status(404).json({
+        error: new Error("Cette sauce n'existe pas"),
+    });
+    };
+    const userLikeIndex = sauce.usersLiked.findIndex(
+    (userId) => userId == req.body.userId
+    );
+    const userDislikeIndex = sauce.usersDisliked.findIndex(
+    (userId) => userId == req.body.userId
+    );
+    if (req.body.like === 1) {
+        if (userLikeIndex !== -1) {
+            return res.status(500).json({
+            error: new Error("L'utilisateur a déjà liké cette sauce"),
+            });
+    }
+        if (userDislikeIndex !== -1) {
+            sauce.usersDisliked.splice(userDislikeIndex, 1);
+            sauce.dislikes--;
+        }
+  sauce.usersLiked.push(req.body.userId);
+  sauce.likes++;
 }
+// Si l'utilisateur a cliqué sur le bouton de dislike
+if (req.body.like === -1) {
+  // Si l'user ID est déjà dans le tableau des dislikes
+  if (userDislikeIndex !== -1) {
+    // On renvoie le code 500 (Internal Server Error) ainsi qu'une nouvelle erreur
+    return res.status(500).json({
+      error: new Error("L'utilisateur a déjà disliké cette sauce"),
+    });
+  }
+  // Si l'user ID est déjà dans le tableau des likes
+  if (userLikeIndex !== -1) {
+    // On supprime l'user ID du tableau des likes et on soustrait 1 au nombre de likes
+    sauce.usersLiked.splice(userLikeIndex, 1);
+    sauce.likes--;
+  }
+  // On ajoute l'user ID dans le tableau des dislikes et on ajoute 1 au nombre de dislikes
+  sauce.usersDisliked.push(req.body.userId);
+  sauce.dislikes++;
+}
+// Si l'utilisateur a annulé son like/dislike
+if (req.body.like === 0) {
+  // Si l'user ID est déjà dans le tableau des dislikes
+  if (userDislikeIndex !== -1) {
+    // On supprime l'user ID du tableau des dislikes et on soustrait 1 au nombre de dislikes
+    sauce.usersDisliked.splice(userDislikeIndex, 1);
+    sauce.dislikes--;
+  }
+  // Si l'user ID est déjà dans le tableau des likes
+  else if (userLikeIndex !== -1) {
+    // On supprime l'user ID du tableau des likes et on soustrait 1 au nombre de likes
+    sauce.usersLiked.splice(userLikeIndex, 1);
+    sauce.likes--;
+  }
+}
+// On met à jour la sauce dont l'id correspond à celui contenu dans les paramètres de la requête
+Sauce.updateOne({ _id: req.params.id }, sauce).then(() => {
+  // On renvoie le code 200 (OK) ainsi qu'un message de confirmation
+  res.status(200).json({ message: "Avis pris en compte" });
+});
+});
+};
--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -6,7 +6,11 @@ const multer = require('../middlewares/multer-config');
 const sauceCtrl = require('../controllers/sauce');
 
 router.get('/', auth, sauceCtrl.getSauces);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.post("/:id/like", auth, sauceCtrl.likeSauce);
 
-//router.post('/', auth, multer, sauceCtrl.createSauce);
 
 module.exports = router;
